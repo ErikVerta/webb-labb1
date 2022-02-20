e281d7a28e8680e9b658afcd0e20751aee51c3d8
ErikVerta <89127095+ErikVerta@users.noreply.github.com>
2022-02-20 17:23:18 +0100

    added front end-code for home page and course page.

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const NavBar = document.getElementById("navbar"); 
+
+function MyFunction() {
+    if (NavBar.style.display == "block") {
+        NavBar.style.display = "none";
+    }
+    else {
+        NavBar.style.display = "block";
+    }
+;}
