76f7e0c96ff154d1f0d038b64792dcd36cf228da
ErikVerta <89127095+ErikVerta@users.noreply.github.com>
2022-02-24 00:59:16 +0100

    added js code to fetch from courses.json and create course elements

--- js/script.js
+++ js/script.js
@@ -1,4 +1,47 @@
-const NavBar = document.getElementById("navbar"); 
+const flexContainer = document.createElement('div');
+
+flexContainer.classList.add('flex-container');
+
+const gridContainer = document.getElementById('grid-container');
+
+gridContainer.appendChild(flexContainer);
+
+fetch("../json/courses.json")
+    .then(function (resp) {
+        return resp.json();
+    })
+    .then(function (data) {
+        data.forEach(function (course) {
+            const cardDiv = document.createElement('div');
+            const cardImg = document.createElement('img');
+            const cardDivOverlay = document.createElement('div');
+            const h4 = document.createElement('h4');
+            const par = document.createElement('p');
+            const a = document.createElement('a');
+
+            cardDiv.classList.add('card');
+            cardDiv.classList.add('flex-item');
+            cardImg.alt = 'Card image';
+            cardImg.classList.add('card-img-top');
+            cardDivOverlay.classList.add('card-img-overlay');
+            h4.classList.add('card-title');
+            par.classList.add('card-text');
+            a.classList.add('btn');
+            a.classList.add('btn-primary');
+
+            h4.appendChild(document.createTextNode(course.courseTitle));
+            par.appendChild(document.createTextNode(course.courseDescription));
+            a.appendChild(document.createTextNode('Buy Course'));
+            cardDivOverlay.appendChild(h4);
+            cardDivOverlay.appendChild(par);
+            cardDivOverlay.appendChild(a);
+            cardDiv.appendChild(cardImg);
+            cardDiv.appendChild(cardDivOverlay);
+            flexContainer.appendChild(cardDiv);
+        })
+    })
+
+    const NavBar = document.getElementById("navbar"); 
 
 function MyFunction() {
     if (NavBar.style.display == "block") {
@@ -9,11 +52,3 @@ function MyFunction() {
     }
     ;
 }
-
-fetch("../json/courses.json")
-    .then(function (resp) {
-        return resp.json();
-    })
-    .then(function (data) {
-        console.log(data[1].courseDescription);
-    })
